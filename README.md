64fab2923fc830f344697c8d2b03eb4470370699
nchao03 <62034613+ChooChooChao@users.noreply.github.com>
2024-05-06 16:48:50 -0700

    Configure Jest and Expose
    Configured the necessary jest items. As well as completed the first part of the expose. Having the photo displayed change based on the selected horn.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 5 - Starter
+Members:
+Nathan Chao - A16785338
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,25 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  // add the photo for the selected horn
+  const imageToDisplay = document.querySelector("img");
+  const selectHorn = document.getElementById("horn-select");
+
+  selectHorn.addEventListener("change", (event) => {
+    let horn = selectHorn.value;
+    switch (horn) {
+      case "air-horn":
+        imageToDisplay.src = "assets/images/air-horn.svg";
+        imageToDisplay.alt = "air-horn";
+        break;
+      case "car-horn":
+        imageToDisplay.src = "assets/images/car-horn.svg";
+        imageToDisplay.alt = "car-horn";
+        break;
+      case "party-horn":
+        imageToDisplay.src = "assets/images/party-horn.svg";
+        imageToDisplay.alt = "party-horn";
+        break;
+    }
+  });
 }

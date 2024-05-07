c6f1fc834f9242c5e613ca0f1428054ad5eed793
nchao03 <62034613+ChooChooChao@users.noreply.github.com>
2024-05-06 18:10:46 -0700

    Select Options
    This code populates the select options using the available voices in the Speech Synthesizer on the page load.

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,32 @@
 // explore.js
 
 window.addEventListener("DOMContentLoaded", init);
+const synth = window.speechSynthesis;
 
 function init() {
-  // TODO
+  // populate the select options on page load
+  let voice = [];
+  const voiceSelect = document.querySelector("select");
+  
+  function populateVoiceList() {
+    voice = synth.getVoices();
+  
+    for (let i = 0; i < voice.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = voice[i].name + " (" + voice[i].lang + ")";
+  
+      if (voice[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voice[i].lang);
+      option.setAttribute("data-name", voice[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
 }
